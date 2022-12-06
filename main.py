bfb313b38971d81c13f98ad65bfec99f8c1c86a4
Anton <shabanov.anton.2012@gmail.com>
2022-12-06 23:21:24 +0500

    removed some imports from main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from Compare import Compare
-import os
-from sys import setrecursionlimit
 
 if __name__ == "__main__":
-    setrecursionlimit(2000)
     a = Compare("1.xml", "2.xml")
     with open("output2.txt", "w") as file:
         _temp = a.CompareFiles()
