ecc39848a23b4706cb42ad75691e9991c60bb4ca
AllowDeny0123 <shabanov.tony@icloud.com>
2022-12-07 09:11:51 +0500

    Added CL Arguments in main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import sys
 from Compare import Compare
 
 if __name__ == "__main__":
-    a = Compare("1.xml", "2.xml")
-    with open("output2.txt", "w") as file:
-        _temp = a.CompareFiles()
-        for i in _temp:
-            file.writelines("".join(i+'\n'))
-        file.close()
-    
-    
+    print(sys.argv)
+    if len(sys.argv) == 3:
+        a = Compare(f"{sys.argv[1]}", f"{sys.argv[2]}")
+        with open("output.output", "w") as file:
+            _temp = a.CompareFiles()
+            for i in _temp:
+                file.writelines("".join(i+'\n'))
+            file.close()
+    else:
+        raise FileNotFoundError("Positional arguments missing!")
+        
+        
