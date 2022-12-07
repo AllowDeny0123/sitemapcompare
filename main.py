93b138d52e23440c80b06185c70511f9aa86505a
AllowDeny0123 <shabanov.tony@icloud.com>
2022-12-07 09:16:27 +0500

    updated CL Args, added output argument as 3-rd pos arg

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import sys
+from File import File
 from Compare import Compare
 
 if __name__ == "__main__":
     print(sys.argv)
     if len(sys.argv) == 3:
         a = Compare(f"{sys.argv[1]}", f"{sys.argv[2]}")
-        with open("output.output", "w") as file:
+        _outputfilename = File("output.output")
+        if sys.argv[3]:
+            _outputfilename = sys.argv[3]
+        with open(f"{_outputfilename}", "w") as file:
             _temp = a.CompareFiles()
             for i in _temp:
                 file.writelines("".join(i+'\n'))
