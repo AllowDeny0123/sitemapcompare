62b715acf238d1a03ceb0557cdaac9c631e3ef46
AllowDeny0123 <shabanov.tony@icloud.com>
2022-12-07 09:18:03 +0500

    fixed _outputname of default path

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     print(sys.argv)
     if len(sys.argv) == 3:
         a = Compare(f"{sys.argv[1]}", f"{sys.argv[2]}")
-        _outputfilename = File("output.output")
+        _outputfilename = File("output.output").path
         if sys.argv[3]:
             _outputfilename = sys.argv[3]
         with open(f"{_outputfilename}", "w") as file:
